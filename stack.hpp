adacbdf5d6ac4a9ff4f485eb53f1c1c92627dbed
Neil Ding <neilding@Neils-MacBook-Pro.local>
2022-11-12 17:39:55 -0800

    add comments

--- stack.hpp
+++ stack.hpp
@@ -27,9 +27,11 @@ namespace ENSC251_Lab4{
         	~Stack();
 			
 			void operator=(Stack& obj);
-			
+			//find the peak of the stack
         	StackFramePtr<T> peek();
 			void push(T the_symbol); 
+
+			//push a value to the bottom of the stack
 			void push_bot(T the_symbol);
 			T pop();
 			bool empty() const;
